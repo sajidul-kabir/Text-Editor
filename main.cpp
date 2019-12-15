9e1c45c62e6f8af93cbe9594f10e167506c00b87
saidul-baker <53114757+saidul-baker@users.noreply.github.com>
2019-12-16 01:12:01 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "mainwindow.h"
+#include "highlighter.h"
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
